66c0b85bb9412374fa7bc80119cedec3a232f4b7
CamMcAlpine <145401570+CamMcAlpine@users.noreply.github.com>
2024-03-13 18:19:29 -0400

    Made lines connecting pairs of markers

--- js/map.js
+++ js/map.js
@@ -7,10 +7,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 map.removeControl(map.attributionControl);
 map.removeControl(map.zoomControl);
 
+var latlngs = [[]];
+var lines = 0;
+var markerNum = 0;
+var polyline = null;
+
 var popup = L.popup();
 
 function onMapClick(e) {
-    var marker = L.marker(e.latlng).addTo(map);
+    var marker = L.marker(e.latlng, {draggable: true}).addTo(map);
+    latlngs[lines].push(marker.getLatLng());
+    marker.on('move', updateLine);
+    markerNum = (markerNum + 1) % 2;
+    if(markerNum == 0){
+        lines++;
+        latlngs.push([]);
+    }
+    if (polyline) {
+        map.removeLayer(polyline);
+    }
+    polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+}
+
+function updateLine() {
+    polyline.setLatLngs(latlngs);
 }
 
 function onCPress(e)

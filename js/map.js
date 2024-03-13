d5bb0257564c848269f9f2195fa900219390bcd8
CamMcAlpine <145401570+CamMcAlpine@users.noreply.github.com>
2024-03-12 20:59:34 -0400

    Map changes
    Removed leaflet and openstreetview attributions
    made map full screen on width <850px

--- js/map.js
+++ js/map.js
@@ -2,9 +2,10 @@ var map = L.map('map').setView([41.62756980846162, -71.00556234602631], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+map.removeControl(map.attributionControl);
+map.removeControl(map.zoomControl);
 
 var popup = L.popup();
 
@@ -17,6 +18,7 @@ function onCPress(e)
     var key = e.key;
     console.log(key);
 }
+
 
 map.on('click', onMapClick);
 map.on('keydown', onCPress)

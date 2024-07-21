70cbd34f17c99f977e6cf7818fc3baf7e7cffdb7
camadam1226123 <cmcalpine@umassd.edu>
2024-07-21 13:43:17 -0400

    Added redirection for pages

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@
 var nameFormDB = firebase.database().ref("nameForm");
 
 document.getElementById("nameForm").addEventListener("submit", submitForm);
+
+var isTeamsGeneratedRef = firebase.database().ref("isTeamsGenerated");
+
+// Function to check for team generation flag and redirect
+isTeamsGeneratedRef.on("value", (snapshot) => {
+    const isGenerated = snapshot.val();
+    if (isGenerated && window.location.pathname != "/admin.html") {
+        window.location.href = "pair.html";
+    }
+});
